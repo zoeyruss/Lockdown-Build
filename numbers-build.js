9f3758c403ce6b5a932959312196a6441ccc92ea
Zoey <zoeyruss@gmail.com>
2020-05-02 21:17:36 -0400

    rotated letters

--- numbers-build.js
+++ numbers-build.js
@@ -1,197 +1,264 @@
+let myFont;
+let angle = 0;
+/* Controls Postion  of Letters*/
+let Pos9 = 0;
+let Pos8 = Pos9 - 50;
+let Pos7 = Pos8 - 50;
+let Pos6 = Pos7 - 50;
+let Pos5 = Pos6 - 50;
+let Pos4 = Pos5 - 50;
+let Pos3 = Pos4 - 50;
+let Pos2 = Pos3 - 50;
+let Pos1 = Pos2 - 50;
 
-    let myFont;
-    let angle = 0;
-    /* Controls Postion  of Letters*/
-    let Dash8Pos = -10;
-    let N7Pos = Dash8Pos - 40;
-    let W6Pos = N7Pos - 40;
-    let O5Pos = W6Pos - 40;
-    let D5Pos = O5Pos - 40;
-    let K4Pos = D5Pos - 40;
-    let C3Pos = K4Pos - 40;
-    let O2Pos = C3Pos - 40;
-    let L1Pos = O2Pos - 40;
+let L1 = "1";
+let L2 = "2";
+let L3 = "3";
+let L4 = "4";
+let L5 = "5";
+let L6 = "6";
+let L7 = "7";
+let L8 = "8";
+let L9 = "9";
 
-    let L1 = "1";
-    let O2 = "2";
-    let C3 = "3";
-    let K4 = "4";
-    let D5 = "5";
-    let O5 = "6";
-    let W6 = "7";
-    let N7 = "8";
-    let Dash8 = "9";
+/* Controls amount of letter forms around circle*/
+let amountL9 = 90;
+let amountL8 = amountL9 / 1.5;
+let amountL7 = amountL8 / 1.33;
+let amountL6 = amountL7 / 1.25;
+let amountL5 = amountL6 / 1.2;
+let amountL4 = amountL5 / 1.17;
+let amountL3 = amountL4 / 1.14;
+let amountL2 = amountL3 / 1.12;
+let amountL1 = amountL2 /1.11;
 
-    /* Controls amount of letter forms around circle*/
-    let amountDash8 = 90;
-    let amountN7 = amountDash8 / 1.5;
-    let amountW6 = amountN7 / 1.33;
-    let amountO5 = amountW6 / 1.255;
-    let amountD5 = amountO5 / 1.18;
-    let amountK4 = amountD5 / 1.18;
-    let amountC3 = amountK4 / 1.15;
-    let amountO2 = amountC3 / 1.12;
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	colorMode(HSB);
+	textFont("sharp");
+	textSize(50);
+	textAlign(CENTER, CENTER);
+}
 
+function draw() {
+	background(0);
+	fill(255);
 
-    function setup() {
-        createCanvas(windowWidth, windowHeight);
-        colorMode(HSB);
-        textFont("sharp");
-        textSize(40);
-        textAlign(CENTER, CENTER)
-    }
+	/* Open Dash Group*/
+	for (let i = 1; i < 360; i += amountL9) {
+		push();
+		/* center on canvas*/
+		translate(width / 2, height / 2);
+		/* rotate Letters around circle*/
+		rotate(radians(i));
+		/* rotate on click*/
+		rotate(angle);
+		push();
 
-    function draw() {
-        background(0);
-        fill(255);
+	
+		/* test rotate letter upward*/
+		translate(0, 0);
+		/* test rotate letter upward*/
+		rotate(190);
+		/*60 push out*/
+		text(L9, 0, Pos9);
+		pop();
+		pop();
+	}
+	/* Close Dash Group*/
 
-        /* Open Dash Group*/
-        for (let i = 1; i < 360; i += amountDash8) {
-            push();
-            textSize(40);
-            /* center on canvas*/
-            translate(width / 2, height / 2);
-            /* rotate Letters around circle*/
-            rotate(radians(i));
-            /* rotate on click*/
-            rotate(angle);
-            text(Dash8, Dash8Pos, 0);
-            pop();
-        }
-        /* Close Dash Group*/
-  
-  
-  
-        /* Open N Group*/
-        for (let i = 0; i < 360; i += amountN7) {
-            push();
-            /* center on canvas*/
-            translate(width / 2, height / 2);
-            /* rotate Letters around circle*/
-            rotate(radians(i));
-            /* rotate on click*/
-            rotate(angle);
-            push();
-            /* test rotate letter upward*/
-            translate(45, 45);
-            rotate(45);
-            text(N7, N7Pos, -45);
-            pop();
-            pop();
-        }
-        /* Close N Group*/
-  
-  
-        /* Open W Group*/
+	/* Open N Group*/
+	for (let i = 0; i < 360; i += amountL8) {
+		push();
+		/* center on canvas*/
+		translate(width / 2, height / 2);
+		/* rotate Letters around circle*/
+		rotate(radians(i));
+		/* rotate on click*/
+		rotate(angle);
+		push();
 
-        for (let i = 0; i < 360; i += amountW6) {
-            push();
-            /* center on canvas*/
-            translate(width / 2, height / 2);
-            /* rotate Letters around circle*/
-            rotate(radians(i));
-            /* rotate on click*/
-            rotate(angle);
-            text(W6, W6Pos, 0);
-            pop();
-        }
-        /* Close W Group*/
-  
-  
-  
-        /* Open O Group*/
-        for (let i = 0; i < 360; i += amountO5) {
-            push();
-            /* center on canvas*/
-            translate(width / 2, height / 2);
-            /* rotate Letters around circle*/
-            rotate(radians(i));
-            /* rotate on click*/
-            rotate(angle);
-            text(O5, O5Pos, 0);
-            pop();
-        }
-        /* Close O Group*/
-  
-  
-        /* Open D Group*/
-        for (let i = 0; i < 360; i += amountD5) {
-            push();
-            /* center on canvas*/
-            translate(width / 2, height / 2);
-            /* rotate Letters around circle*/
-            rotate(radians(i));
-            /* rotate on click*/
-            rotate(angle);
-            text(D5, D5Pos, 0);
-            pop();
-        }
-        /* Close D Group*/
-  
-  
-        /* Open K Group*/
-        for (let i = 0; i < 360; i += amountK4) {
-            push();
-            /* center on canvas*/
-            translate(width / 2, height / 2);
-            /* rotate Letters around circle*/
-            rotate(radians(i));
-            /* rotate on click*/
-            rotate(angle);
-            text(K4, K4Pos, 0);
-            pop();
-        }
-        /* Close K Group*/
-  
-        /* Open C Group*/
-        for (let i = 0; i < 360; i += amountC3) {
-            push();
-            /* center on canvas*/
-            translate(width / 2, height / 2);
-            /* rotate Letters around circle*/
-            rotate(radians(i));
-            /* rotate on click*/
-            rotate(angle);
-            text(C3, C3Pos, 0);
-      		
-            pop();
-        }
-        /* Close C Group*/
-  
-        /* Open O Group*/
-        for (let i = 0; i < 360; i += amountO2) {
-            push();
-            /* center on canvas*/
-            translate(width / 2, height / 2);
-            /* rotate Letters around circle*/
-            rotate(radians(i));
-            /* rotate on click*/
-            rotate(angle);
-            text(O2, O2Pos, 0);
-            pop();
-        }
-        /* Close O Group*/
-  
-        /* Open L Group*/
-        for (let i = 0; i < 360; i += amountO2) {
-            push();
-            /* center on canvas*/
-            translate(width / 2, height / 2);
-            /* rotate Letters around circle*/
-            rotate(radians(i));
-            /* rotate on click*/
-            rotate(angle);
-            text(L1, L1Pos, 0);
-            pop();
-        }
-        /* Close L Group*/
- 
-    }
+	
+		/* test rotate letter upward*/
+		translate(0, -2);
+		/* test rotate letter upward*/
+		rotate(189);
+		/*60 push out*/
+		text(L8, 0, Pos8);
+		pop();
+		pop();
+	}
+	/* Close N Group*/
 
+	/* Open W Group*/
 
-    function windowResized() {
-        resizeCanvas(windowWidth, windowHeight);
-    }
-    function mousePressed() {
-        angle = angle - 50;
-    }
+	for (let i = 0; i < 360; i += amountL7) {
+		push();
+		/* center on canvas*/
+		translate(width / 2, height / 2);
+		/* rotate Letters around circle*/
+		rotate(radians(i));
+		/* rotate on click*/
+		rotate(angle);
+		push();
+
+		rotate(90);
+		/* test rotate letter upward*/
+		translate(0, 0);
+		/* test rotate letter upward*/
+		rotate(49);
+		/*60 push out*/
+		text(L7, 0, Pos7);
+		pop();
+		pop();
+	}
+	/* Close W Group*/
+
+	/* Open O Group*/
+	for (let i = 0; i < 360; i += amountL6) {
+				push();
+				/* center on canvas*/
+				translate(width / 2, height / 2);
+				/* rotate Letters around circle*/
+				rotate(radians(i));
+				/* rotate on click*/
+				rotate(angle);
+				push();
+
+				rotate(90);
+				/* test rotate letter upward*/
+				translate(0, 0);
+				/* test rotate letter upward*/
+				rotate(90);
+				/*60 push out*/
+				text(L6, 0, Pos6);
+				pop();
+				pop();
+	}
+	/* Close O Group*/
+
+	/* Open D Group*/
+	for (let i = 0; i < 360; i += amountL5) {
+		push();
+		/* center on canvas*/
+		translate(width / 2, height / 2);
+		/* rotate Letters around circle*/
+		rotate(radians(i));
+		/* rotate on click*/
+		rotate(angle);
+		push();
+
+		rotate(89.55);
+		/* test rotate letter upward*/
+		translate(0, 0);
+		/* test rotate letter upward*/
+	
+		/*60 push out*/
+		text(L5, 0, Pos5);
+		pop();
+		pop();
+	}
+	/* Close D Group*/
+
+	/* Open K Group*/
+	for (let i = 0; i < 360; i += amountL4) {
+
+		push();
+		/* center on canvas*/
+		translate(width / 2, height / 2);
+		/* rotate Letters around circle*/
+		rotate(radians(i));
+		/* rotate on click*/
+		rotate(angle);
+		push();
+
+        rotate(90);
+		/* test rotate letter upward*/
+		translate(0, 0);
+		/* test rotate letter upward*/
+	
+		/*60 push out*/
+		text(L4, 0, Pos4);
+		pop();
+		pop();
+	}
+	/* Close K Group*/
+
+	/* Open C Group*/
+	for (let i = 0; i < 360; i += amountL3) {
+	
+		push();
+		/* center on canvas*/
+		translate(width / 2, height / 2);
+		/* rotate Letters around circle*/
+		rotate(radians(i));
+		/* rotate on click*/
+		rotate(angle);
+		push();
+
+		rotate(89.55);
+		/* test rotate letter upward*/
+		translate(0, 0);
+		/* test rotate letter upward*/
+
+		/*60 push out*/
+		text(L3, 0, Pos3);
+		pop();
+		pop();
+	}
+	/* Close C Group*/
+
+	/* Open O Group*/
+	for (let i = 0; i < 360; i += amountL2) {
+			push();
+			/* center on canvas*/
+			translate(width / 2, height / 2);
+			/* rotate Letters around circle*/
+			rotate(radians(i));
+			/* rotate on click*/
+			rotate(angle);
+			push();
+
+			rotate(89.55);
+			/* test rotate letter upward*/
+			translate(0, 0);
+			/* test rotate letter upward*/
+
+			/*60 push out*/
+			text(L2, 0, Pos2);
+			pop();
+			pop();
+	}
+	/* Close O Group*/
+
+	/* Open L Group*/
+	for (let i = 0; i < 360; i += amountL1) {
+			push();
+			/* center on canvas*/
+			translate(width / 2, height / 2);
+			/* rotate Letters around circle*/
+			rotate(radians(i));
+			/* rotate on click*/
+			rotate(angle);
+			push();
+
+			rotate(89.55);
+			/* test rotate letter upward*/
+			translate(0, 0);
+			/* test rotate letter upward*/
+
+			/*60 push out*/
+			text(L1, 0, Pos1);
+			pop();
+        pop();
+        
+	}
+	/* Close L Group*/
+}
+
+function windowResized() {
+	resizeCanvas(windowWidth, windowHeight);
+}
+function mousePressed() {
+	angle = angle - 50;
+}
